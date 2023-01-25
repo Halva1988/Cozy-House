2c13da2d518cd29738e78cf660f84c1052213b68
Aleksandr Khalaim <Khalaimaleksandr@gmail.com>
2023-01-25 23:19:51 +0300

    created modal

--- src/js/script.js
+++ src/js/script.js
@@ -1,18 +1,25 @@
 const swiper = new Swiper('.swiper', {
-    slidesPerView: 3,
-    navigation: {
-        nextEl: '.arrow-right',
-        prevEl: '.arrow-left',
+  slidesPerView: 3,
+  navigation: {
+    nextEl: '.arrow-right',
+    prevEl: '.arrow-left',
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 1
     },
-    breakpoints: {
-        320: {
-            slidesPerView: 1
-        },
-        768: {
-            slidesPerView: 2
-        },
-        1280: {
-            slidesPerView: 3
-        },
-    }
+    768: {
+      slidesPerView: 2
+    },
+    1280: {
+      slidesPerView: 3
+    },
+  }
 })
+
+const pets = document.querySelectorAll('.pets__card');
+const dogName = ['Katrine', 'Jennifer', 'Woody', 'Sophia', 'Timmy', 'Charly', 'Scarlet', 'Freddie'];
+
+for(let i = 0; i < pets.length; i++) {
+  pets[i].setAttribute('id', `${dogName[i]}`)
+}
