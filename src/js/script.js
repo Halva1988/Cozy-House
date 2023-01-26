786457fe774237ce8409d5d9f2fb89d3a0c0c2a9
Aleksandr Khalaim <Khalaimaleksandr@gmail.com>
2023-01-26 17:42:37 +0300

    added modal, pets.json, logic modal, style modal tablet & mobile

--- src/js/script.js
+++ src/js/script.js
@@ -19,7 +19,35 @@ const swiper = new Swiper('.swiper', {
 
 const pets = document.querySelectorAll('.pets__card');
 const dogName = ['Katrine', 'Jennifer', 'Woody', 'Sophia', 'Timmy', 'Charly', 'Scarlet', 'Freddie'];
+const modal = document.querySelector('.modal')
+const closeModal = document.querySelector('.modal__close-button')
 
-for(let i = 0; i < pets.length; i++) {
+for (let i = 0; i < pets.length; i++) {
   pets[i].setAttribute('id', `${dogName[i]}`)
 }
+
+const learn = document.querySelectorAll('.learn');
+
+learn.forEach(element => element.addEventListener('click', (e) => {
+  let card = e.target.closest('.pets__card');
+  fetch('../../dataBase/pets.txt')
+  .then(response => response.json())
+  .then(result => {
+    result.map(pet => {
+      if (pet.name === card.id) {
+        modal.style.display = 'block'
+        modal.querySelector('.modal__img').setAttribute('src', `${pet.img}`)
+        modal.querySelector('.modal__pet-name').innerHTML = pet.name
+        modal.querySelector('.modal__pet-breed').innerHTML = pet.breed
+        modal.querySelector('.modal__pet-description').innerHTML = pet.desc
+      }
+    })
+  })
+  .catch(err => {
+    console.log("error: ", err);
+  })
+}))
+
+closeModal.addEventListener('click', () => {
+  modal.style.display = 'none'
+})
