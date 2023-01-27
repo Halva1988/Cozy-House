e20b57eb08ab826bf2d613c787eb925dd2db5cd2
Aleksandr Khalaim <Khalaimaleksandr@gmail.com>
2023-01-27 19:20:05 +0300

    added burger menu to mobile width

--- src/js/script.js
+++ src/js/script.js
@@ -17,6 +17,14 @@ const swiper = new Swiper('.swiper', {
   }
 })
 
+const burger = document.querySelector('.burger')
+const navigation = document.querySelector('.header__navigation')
+
+burger.addEventListener('click', () => {
+  burger.classList.toggle('burger_pressed')
+  navigation.classList.toggle('header__navigation_burgerIsActive')
+})
+
 const pets = document.querySelectorAll('.pets__card');
 const dogName = ['Katrine', 'Jennifer', 'Woody', 'Sophia', 'Timmy', 'Charly', 'Scarlet', 'Freddie'];
 const modal = document.querySelector('.modal')
@@ -31,21 +39,21 @@ const learn = document.querySelectorAll('.learn');
 learn.forEach(element => element.addEventListener('click', (e) => {
   let card = e.target.closest('.pets__card');
   fetch('../../dataBase/pets.txt')
-  .then(response => response.json())
-  .then(result => {
-    result.map(pet => {
-      if (pet.name === card.id) {
-        modal.style.display = 'block'
-        modal.querySelector('.modal__img').setAttribute('src', `${pet.img}`)
-        modal.querySelector('.modal__pet-name').innerHTML = pet.name
-        modal.querySelector('.modal__pet-breed').innerHTML = pet.breed
-        modal.querySelector('.modal__pet-description').innerHTML = pet.desc
-      }
+    .then(response => response.json())
+    .then(result => {
+      result.map(pet => {
+        if (pet.name === card.id) {
+          modal.style.display = 'block'
+          modal.querySelector('.modal__img').setAttribute('src', `${pet.img}`)
+          modal.querySelector('.modal__pet-name').innerHTML = pet.name
+          modal.querySelector('.modal__pet-breed').innerHTML = pet.breed
+          modal.querySelector('.modal__pet-description').innerHTML = pet.desc
+        }
+      })
     })
-  })
-  .catch(err => {
-    console.log("error: ", err);
-  })
+    .catch(err => {
+      console.log("error: ", err);
+    })
 }))
 
 closeModal.addEventListener('click', () => {
