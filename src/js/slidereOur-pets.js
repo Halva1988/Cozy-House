8ec577120bc6754a0aad2c951b5948c1a4be91da
Aleksander <61146913+Halva1988@users.noreply.github.com>
2023-01-31 17:18:00 +0300

    Update slidereOur-pets.js
    Test

--- src/js/slidereOur-pets.js
+++ src/js/slidereOur-pets.js
@@ -28,7 +28,7 @@ const swiper = new Swiper('.swiper', {
 const next = document.querySelector('.next')
 const prev = document.querySelector('.prev')
 const numberSlide = document.querySelector('.page-number')
-numberSlide.textContent = 1
+numberSlide.textContent = 0
 
 next.addEventListener('click', () => {
   numberSlide.textContent = parseInt(numberSlide.textContent) + 1
